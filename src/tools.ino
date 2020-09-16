8ec1440c4b655b79ba7eaf8d17a3523558e5ac8e
zeuros <thrashpoubelle@gmail.com>
2020-09-17 01:44:10 +0200

    add smoothing amount

--- src/tools.ino
+++ src/tools.ino
@@ -1,8 +1,8 @@
 
 // ajoute une valeur au tableau de moyennes et retourne la moyenne des 10 dernières valeurs.
 // évite les sautes de valeurs dues à des perturbations
-int smooth(int avg, int newValue) {
-    return ( avg * 9 + newValue ) / 10;
+int smooth(int avg, int newValue, int amount = 20) {
+    return ( avg * (amount - 1) + newValue ) / amount;
 }
 
 int sum(int* arr, int size) {
